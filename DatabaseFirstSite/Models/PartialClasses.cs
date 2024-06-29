1b6f3e82edd0e547f2799958565cd855d8335e25
0910QiChen <0910qichen@gmail.com>
2024-06-29 00:36:46 -0400

    Add project files.

---
+++ DatabaseFirstSite/Models/PartialClasses.cs
@@ -0,0 +1,16 @@
+﻿using DatabaseFirstSite.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatabaseFirstSite.Models
+{
+    [MetadataType(typeof(StudentMetadata))]
+    public partial class Student
+    {
+    }
+
+    [MetadataType(typeof(EnrollmentMetadata))]
+    public partial class Enrollment
+    {
+    }
+}
